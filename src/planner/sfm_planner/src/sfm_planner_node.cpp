0e15c2b4d45608691495438c739b7985caeafb13
Guo Zhanyu <942568052@qq.com>
2023-04-20 16:57:08 +0800

    add sfm, todo

--- src/planner/sfm_planner/src/sfm_planner_node.cpp
+++ src/planner/sfm_planner/src/sfm_planner_node.cpp
@@ -2,6 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-
+    // a simple way to valid sfm-model
+    // disable ego_planner
+    // enable sfm_planner
+    // publish quadrotor_msgs::PositionCommand with update x, y, z to /position_cmd
+    // centerlized control first: all position known, all controllable...
     return 0;
 }
